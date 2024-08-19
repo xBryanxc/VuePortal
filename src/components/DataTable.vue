c65e149304f09039f24e6b5429a832706782ce02
Bryan Zúñiga M <bzuniga@gpgsa.net>
2024-08-19 12:18:44 -0600

    feat: Actualizar nombres de columnas en DataTable.vue
    Se actualizaron los nombres de las columnas en el componente DataTable.vue para reflejar los cambios en el diseño. Los nombres de las columnas "ID Tarea", "ID Proceso", "Estado", "Proceso", "Nombre empresa" y "Origen" se cambiaron a "Id", "Trámite", "Fase", "Referencia base", "Id Solicitante" y "Descripción Solicitante" respectivamente. También se eliminó la columna "Proyecto". Además, se agregaron íconos visuales para indicar si una tarea está vencida, por vencer o a tiempo.

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -14,31 +14,41 @@
         <table class="w-full text-sm text-left text-gray-500">
             <thead class="text-xs text-gray-700 uppercase bg-gray-50">
                 <tr>
-                    <th class="px-4 py-3">ID Tarea</th>
-                    <th class="px-4 py-3">ID Proceso</th>
-                    <th class="px-4 py-3">Estado</th>
-                    <th class="px-4 py-3">Proceso</th>
+                    <th class="px-4 py-3">Id</th>
+                    <th class="px-4 py-3">Trámite</th>
+                    <th class="px-4 py-3">Fase</th>
                     <th class="px-4 py-3">Referencia base</th>
-                    <th class="px-4 py-3">Nombre empresa</th>
+                    <th class="px-4 py-3">Id Solicitante</th>
+                    <th class="px-4 py-3">Descripción Solicitante</th>
+                    <th class="px-4 py-3">Fecha de llegada</th>
                     <th class="px-4 py-3">Fecha de vencimiento</th>
-                    <th class="px-4 py-3">Origen</th>
                     <th class="px-4 py-3"></th>
                 </tr>
             </thead>
             <tbody>
                 <tr v-for="item in resultadosFiltrados" :key="item.TaskId" class="border-b cursor-pointer" @click="abrirIframe(item.TaskId ?? 0, item.Project ?? 'VUI')">
                     <td class="px-4 py-3 font-medium text-gray-900">{{ item.TaskId }}</td>
-                    <td class="px-4 py-3 font-medium text-gray-900">{{ item.ProcessId }}</td>
-                    <td class="px-4 py-3 font-medium text-gray-900">{{ item.StatusName }}</td>
                     <td class="px-4 py-3 font-medium text-gray-900">{{ item.ProcessClassName }}</td>
+                    <td class="px-4 py-3 font-medium text-gray-900">{{ item.StatusName }}</td>
                     <td class="px-4 py-3 font-medium text-gray-900">{{ item.ProcessBaseReference }}</td>
+                    <td class="px-4 py-3 font-medium text-gray-900">{{ 1111111 }}</td>
                     <td class="px-4 py-3 font-medium text-gray-900">{{ item.ProcessTheme1 }}</td>
+                    <td class="px-4 py-3 font-medium text-gray-900">{{ formatFecha(item.ArrivalDate ?? new Date()) }}</td>
                     <td class="px-4 py-3 font-medium text-gray-900">{{ formatFecha(item.Deadline ?? new Date()) }}</td>
-                    <td class="px-4 py-3 font-medium text-gray-900">{{ item.Project }}</td>
                     <td class="px-4 py-3">
-                        <span v-if="comprobarVencimiento(item.Deadline ?? new Date())" class="text-red-500 cursor-pointer" title="VENCIDA">
+                        <span v-if="comprobarVencimiento(item.Deadline ?? new Date()) == 'vencida'" class="text-red-500 cursor-pointer" title="VENCIDA">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                                 <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM9 7a1 1 0 012 0v4a1 1 0 11-2 0V7zm1 9a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd" />
+                            </svg>
+                        </span>
+                        <span v-else-if="comprobarVencimiento(item.Deadline ?? new Date()) == 'casi_vencida'" class="text-yellow-500 cursor-pointer" title="POR VENCER">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                <path fill-rule="evenodd" d="M4 10h12a1 1 0 110 2H4a1 1 0 110-2z" clip-rule="evenodd" />
+                            </svg>
+                        </span>
+                        <span v-else-if="comprobarVencimiento(item.Deadline ?? new Date()) == 'en_tiempo'" class="text-green-500 cursor-pointer" title="A TIEMPO">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 00-1.414 0L8 12.586l-2.293-2.293a1 1 0 00-1.414 1.414l3 3a1 1 0 001.414 0l8-8a1 1 0 000-1.414z" clip-rule="evenodd" />
                             </svg>
                         </span>
                     </td>
@@ -123,15 +133,27 @@ const formatFecha = (date: Date) => {
     const dia = fecha.getDate();
     const mes = fecha.getMonth() + 1;
     const anio = fecha.getFullYear();
-
-    return `${dia}/${mes}/${anio}`;
+    // if fecha  is 1/1/1900 return empty string
+    return dia === 1 && mes === 1 && anio === 1900 ? "" : `${dia}/${mes}/${anio}`;
 }
 
 const comprobarVencimiento = (date: Date) => {
     const fecha = new Date(date);
     const hoy = new Date();
+    const casiVencida = new Date(fecha);
+    casiVencida.setDate(casiVencida.getDate() - 90);
 
-    return fecha < hoy ? true : false;
+    if (fecha.getDate() === 1 && fecha.getMonth() === 0 && fecha.getFullYear() === 1900){
+        return "sin venimiento";
+    }
+
+    if (fecha < hoy){
+        return "vencida";
+    } else if (casiVencida <= hoy){
+        return "casi_vencida";
+    } else {
+        return "en_tiempo";
+    }
 }
 
 const administrarFiltrosCheckbox = (value: string) => {
